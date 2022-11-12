cd35eec915171700040346f9fdbdc0a6bb4f3c9d
benpung <benpung3@gmail.com>
2022-11-12 15:50:07 -0500

    message

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from dataset import getDataset
+from model import create_model
 
-print('hello"')
+directory = "images"
+fileLocation = "images/vehicle_images/vehicle_images"
+
+training, test = getDataset(directory)
+
+model=create_model(input_shape=(180,180)+(3,), num_classes=2)
